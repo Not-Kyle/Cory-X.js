8fc65b81c2dc96b3a80589211ae331857705bf22
/Immortal <77679412+Not-Kyle@users.noreply.github.com>
2023-05-20 08:15:59 -0400

    Update Cory-X-Var.js

--- Cory-X-Var.js
+++ Cory-X-Var.js
@@ -1,1 +1,14 @@
+var nameOne = "Cory";
+var nameTwo = " - X";
 
+console.log(nameOne, nameTwo);
+
+let http18 = "IP Display";
+console.log(http18);
+
+const consent3 = "Contents";
+console.log(consent3);
+
+var a
+a = 8
+a++
