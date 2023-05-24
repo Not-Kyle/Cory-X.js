9cd0c94323c5818e91582c5fa570c2cf8adc1685
/Immortal <77679412+Not-Kyle@users.noreply.github.com>
2023-05-23 21:04:30 -0400

    Update ShadowSystem.js

--- ShadowSystem.js
+++ ShadowSystem.js
@@ -1,6 +1,12 @@
-const remainder = 9 % 10;
-const remainder2A = 2 % 11;
-console.log(remainder, remainder2A);
+let rando1 = Math.floor(Math.random()*255);
+const strings1 = ".";
+let rando2 = Math.floor(Math.random()*255);
+const strings2 = ".";
+let rando3 = Math.floor(Math.random()*255);
+const strings3 = ".";
+let rando4 = Math.floor(Math.random()*255);
+
+console.log(rando1,strings1,rando2,strings2,rando3,strings3,rando4);
 
 const ddoslinko = '<a href="http://www.netscout.com/ddos-attack-map" target="_blank">Link</a>';
 
