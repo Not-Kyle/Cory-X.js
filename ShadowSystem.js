eb65bdf364765d24aa926abb92dc1adb31aa341a
/Immortal <77679412+Not-Kyle@users.noreply.github.com>
2023-05-23 21:17:00 -0400

    Update ShadowSystem.js

--- ShadowSystem.js
+++ ShadowSystem.js
@@ -3,10 +3,12 @@ const strings1 = ".";
 let rando2 = Math.floor(Math.random()*255);
 const strings2 = ".";
 let rando3 = Math.floor(Math.random()*255);
-const strings3 = ".";
+const strings3 = ".";                               //gotta clean up these lines
 let rando4 = Math.floor(Math.random()*255);
 
 console.log(rando1,strings1,rando2,strings2,rando3,strings3,rando4);
+
+
 
 const ddoslinko = '<a href="http://www.netscout.com/ddos-attack-map" target="_blank">Link</a>';
 
