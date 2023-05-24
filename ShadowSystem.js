fd2c653d51dcdd0425ea625f335adea2d2d8d165
/Immortal <77679412+Not-Kyle@users.noreply.github.com>
2023-05-23 20:54:19 -0400

    Update ShadowSystem.js

--- ShadowSystem.js
+++ ShadowSystem.js
@@ -2,5 +2,6 @@ const remainder = 9 % 10;
 const remainder2A = 2 % 11;
 console.log(remainder, remainder2A);
 
+const ddoslinko = '<a href="http://www.netscout.com/ddos-attack-map" target="_blank">Link</a>';
 
 
