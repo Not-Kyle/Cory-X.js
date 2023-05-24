72c73538be69e223de21111ab2a05ed9ba2fe23b
/Immortal <77679412+Not-Kyle@users.noreply.github.com>
2023-05-24 14:25:29 -0400

    Update WHY AM I STILL IN LOS ANGELES.js

--- WHY AM I STILL IN LOS ANGELES.js
+++ WHY AM I STILL IN LOS ANGELES.js
@@ -1,1 +1,13 @@
+const nextlinetext = "WHY\nAM\nI\nSTILL\nIN\nLA"; //yooo im skidding!!
+console.log(nextlinetext)
 
+const teststring = 'Big\n\tFat\n\t\tNut';
+console.log(teststring);
+
+const nextlinetext3 = "IM\nOF\nLOS\nANGELES";
+console.log(nextlinetext3)
+
+const nextlinetext2 = "WHY\nAM\nI\nSTILL\nIN\nLA";
+console.log(nextlinetext2)
+
+//this page is for practice, so i don't lose the knowledge
