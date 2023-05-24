958c22229d8e618a86d27f13dcda9848e0a0232a
/Immortal <77679412+Not-Kyle@users.noreply.github.com>
2023-05-24 15:16:00 -0400

    Update WHY AM I STILL IN LOS ANGELES.js

--- WHY AM I STILL IN LOS ANGELES.js
+++ WHY AM I STILL IN LOS ANGELES.js
@@ -1,5 +1,12 @@
 var namelol = "Shadow! ";
-
+var firstsong = "YEAH RIGHT";
+var secondsong = "WANTED U";
+var thirdsong = "Sanctuary";
+var fourthsong = "TEST DRIVE";
+var fifthsong = "WHY AM I STILL IN LA";
+var sixthsong = "I don't wanna waste my time";
+var seventhsong = "worldstar money (interlude)";
+var eighthsong = "YUKON (INTERLUDE)";
 
 const nextlinetext = "WHY\nAM\nI\nSTILL\nIN\nLA"; //yooo im skidding!!
 console.log(nextlinetext)
@@ -17,4 +24,9 @@ console.log(nextlinetext2)
 var thestring = "Cory is old lets change the name, " + namelol + "Thats better, lol! ";  //this is not my new name lol, cory is legendary
 
 console.log(thestring);
+
+var songstring = "1st. " + firstsong + "\n 2nd. " + secondsong + "\n 3rd. " + thirdsong + "\n 4th. " + fourthsong + "\n 5th. " + fifthsong + "\n 6th. " + sixthsong + "\n 7th. " + seventhsong + "\n 8th. " + eighthsong + "";
+console.log(songstring)
+
+
 
