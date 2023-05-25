15e47e28830efbed9e06a15a1e60646b98b0c5b5
/Immortal <77679412+Not-Kyle@users.noreply.github.com>
2023-05-25 15:11:43 -0400

    Update Array.js

--- Array.js
+++ Array.js
@@ -12,3 +12,14 @@ console.log(array2);
 let amd = 80 * 0.5;
 console.log(amd)  //mb, was researching AMD Stocks
 
+const arr3 = ["Javascript", "SQL", "Lua", "C", "C++", "Ruby", "HTML"];
+
+arr3[0] = "Javascript";
+arr3[1] = "SQL";
+arr3[2] = "Ruby";
+arr3[3] = "C++";
+arr3[4] = "C";
+arr3[5] = "HTML";
+arr3[6] = "Lua";
+
+console.log(arr3);
